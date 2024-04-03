c0a71c962c4d19585cb6b0874e3e2e2db003df24
ALCIDIANO FERREIRA SOARES <alcidiano.fsoares@senacsp.edu.br>
2024-04-03 17:19:11 -0300

    03-04-js-02

---
+++ eventos/js/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector
+("#my-buttom")
+
+btn.addEventListener("click", function(){
+    console.log("Clicou")
+})
+
+// Test
+
+const secondBtn = document.querySelector("#btn");
+function imprimirMensagem(){
+    console.log("Teste")
+}
+secondBtn.addEventListener("click", imprimirMensagem);
+
+
+const thirdBtn = document.querySelector("#other-btn");
+thirdBtn.addEventListener("click", () => {
+    console.log("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});
